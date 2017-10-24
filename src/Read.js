6d0cbdc5dd0d9ce913d768ed7aaa43567555f1e6
Graham Earley <graham.p.earley@gmail.com>
2017-10-24 16:16:48 +0530

    Add method for reading documents

---
+++ src/Read.js
@@ -0,0 +1,11 @@
+function getDocuments(path, email, key, projectId) {  
+  const token = getAuthToken_(email, key);
+    
+  const baseUrl = "https://firestore.googleapis.com/v1beta1/projects/" + projectId + "/databases/(default)/documents/" + path;
+  const options = {
+   'muteHttpExceptions' : true,
+   'headers': {'content-type': 'application/json', 'Authorization': 'Bearer ' + token}
+  };
+  
+  return getObjectFromResponse(UrlFetchApp.fetch(baseUrl, options));
+}
