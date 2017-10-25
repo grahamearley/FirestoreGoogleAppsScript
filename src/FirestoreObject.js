043e68ab7f24c27114bede0cf63c738686464c49
Graham Earley <graham.p.earley@gmail.com>
2017-10-25 18:04:41 +0530

    Fix silly typos and reformat spacing

--- src/FirestoreObject.js
+++ src/FirestoreObject.js
@@ -5,19 +5,19 @@
  * @return {object} a Firestore document with the given fields
  */
 function createFirestoreDocument(fields) {
-  const keys = Object.keys(fields);
-  const firestoreObj = {};
-    
-  firestoreObj["fields"] = {};
-    
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    var val = fields[key];
-  
-    firestoreObj["fields"][key] = wrapValue_(val);
-  }
+    const keys = Object.keys(fields);
+    const firestoreObj = {};
 
-  return firestoreObj;
+    firestoreObj["fields"] = {};
+
+    for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        var val = fields[key];
+
+        firestoreObj["fields"][key] = wrapValue_(val);
+    }
+
+    return firestoreObj;
 }
 
 /**
@@ -27,114 +27,118 @@ function createFirestoreDocument(fields) {
  * @return {object} an object with the given document's fields and values
  */
 function getFieldsFromFirestoreDocument(firestoreDoc) {
-  const fields = firestoreDoc["fields"];
-  const keys = Object.keys(fields);
-  const object = {};
+    if (!firestoreDoc || !firestoreDoc["fields"]) {
+        return {};
+    }
 
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    var firestoreValue = fields[key];
-  
-    object[key] = unwrapValue_(firestoreValue);
-  }
+    const fields = firestoreDoc["fields"];
+    const keys = Object.keys(fields);
+    const object = {};
 
-  return object;
+    for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        var firestoreValue = fields[key];
+
+        object[key] = unwrapValue_(firestoreValue);
+    }
+
+    return object;
 }
 
 function wrapValue_(value) {
-  var type = typeof(value);
-  switch(type) {
-      case "string":
-        return wrapString_(value);
-      case "object":
-        return wrapObject_(value);
-      case "number":
-        return wrapNumber_(value);
-      case "boolean":
-        return wrapBoolean_(value);
-      default:
-        // error
-        return null;
+    var type = typeof(value);
+    switch (type) {
+        case "string":
+            return wrapString_(value);
+        case "object":
+            return wrapObject_(value);
+        case "number":
+            return wrapNumber_(value);
+        case "boolean":
+            return wrapBoolean_(value);
+        default:
+            // error
+            return null;
     }
 }
 
 function unwrapValue_(value) {
-  var type = Object.keys(value)[0];
-  switch(type) {
-      case "stringValue":
-      case "boolean":
-      case "integerValue":
-      case "doubleValue":
-        return value[type];
-      case "nullValue":
-        return null;
-      case "mapVaue":
-        return getFieldsFromFirestoreDocument(value[type]);
-      case "arrayValue":
-        return unwrapArray_(value[type]["values"]);
-      default:
-        // error
-        return null;
+    var type = Object.keys(value)[0];
+    switch (type) {
+        case "stringValue":
+        case "booleanValue":
+        case "integerValue":
+        case "doubleValue":
+            return value[type];
+        case "nullValue":
+            return null;
+        case "mapValue":
+            return getFieldsFromFirestoreDocument(value[type]);
+        case "arrayValue":
+            return unwrapArray_(value[type]["values"]);
+        default:
+            // error
+            return null;
     }
 }
 
 function wrapString_(string) {
-  return {"stringValue" : string};
+    return {"stringValue": string};
 }
 
 function wrapObject_(object) {
-  
-  if (!object) {
-    return {"nullValue": null};
-  }
 
-  if (Array.isArray(object)) {
-    return wrapArray_(object);
-  }
-  
-  return {"mapValue" : createFirestoreDocument(object)};
+    if (!object) {
+        return {"nullValue": null};
+    }
+
+    if (Array.isArray(object)) {
+        return wrapArray_(object);
+    }
+
+    return {"mapValue": createFirestoreDocument(object)};
 }
 
 function wrapNumber_(num) {
-  if (isInt_(num)) {
-    return wrapInt_(num);
-  } else {
-    return wrapDouble_(num);
-  }
+    if (isInt_(num)) {
+        return wrapInt_(num);
+    } else {
+        return wrapDouble_(num);
+    }
 }
 
 function wrapInt_(int) {
-  return {"integerValue" : int};
+    return {"integerValue": int};
 }
 
 function wrapDouble_(double) {
-  return {"doubleValue" : double};
+    return {"doubleValue": double};
 }
 
 function wrapBoolean_(boolean) {
-  return {"booleanValue" : boolean};
+    return {"booleanValue": boolean};
 }
 
 function wrapArray_(array) {
-  const wrappedArray = [];
+    const wrappedArray = [];
 
-  for (var i = 0; i < array.length; i++) {
-    var value = array[i];
-    var wrappedValue = wrapValue_(value);
-    wrappedArray.push(wrappedValue);
-  }
+    for (var i = 0; i < array.length; i++) {
+        var value = array[i];
+        var wrappedValue = wrapValue_(value);
+        wrappedArray.push(wrappedValue);
+    }
 
-  return {"arrayValue" : {"values": wrappedArray}};
+    return {"arrayValue": {"values": wrappedArray}};
 }
 
 function unwrapArray_(wrappedArray) {
-  const array = [];
+    const array = [];
 
-  for (var i = 0; i < wrappedArray.length; i++) {
-    var wrappedValue = wrappedArray[i];
-    var unwrappedValue = unwrapValue_(wrappedValue);
-    array.push(unwrappedValue);
-  }
+    for (var i = 0; i < wrappedArray.length; i++) {
+        var wrappedValue = wrappedArray[i];
+        var unwrappedValue = unwrapValue_(wrappedValue);
+        array.push(unwrappedValue);
+    }
 
-  return array;
+    return array;
 }
